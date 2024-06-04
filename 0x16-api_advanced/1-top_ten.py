75eac9f25246fdc043051beda65cf020435773fb
Frank Anin <frankanin99@gmail.com>
2024-06-04 23:53:00 +0000

    advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,12 @@ def top_ten(subreddit):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if not posts:
-            print("None")
-        else:
-            for post in posts:
-                print(post['data']['title'])
-    else:
+    if response.status_code == 404:
         print("None")
+        return
+
+    data = response.json()
+    posts = data['data']['children']
+
+    for post in posts:
+        print(post['data']['title'])
