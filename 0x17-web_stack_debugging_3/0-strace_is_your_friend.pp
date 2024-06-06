052f58e8daa84741806945af13c32609774f8a14
Frank Anin <frankanin99@gmail.com>
2024-06-06 03:13:25 +0000

    debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
-# Ensure WordPress settings file is configured properly
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  source  => 'puppet:///path/to/wp-settings.php', # Replace with the actual source path
-  owner   => 'www-data',  # Replace with the appropriate owner
-  group   => 'www-data',  # Replace with the appropriate group
-  mode    => '0644',      # Adjust permissions as needed
-  notify  => Exec['fix-wordpress'],  # Trigger fix command after file update
-}
+# Automate fix using Puppet
 
-# Automate fix using Puppet exec
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/usr/bin', '/bin'], # Specify full path to binaries
-  refreshonly => true,  # Only run when triggered by file change
+  path    => '/usr/local/bin/:/bin/'
 }
