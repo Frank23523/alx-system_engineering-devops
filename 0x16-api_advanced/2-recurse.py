86a05ed2c8c5045f74ebd82da87c51fa6401ce19
Frank Anin <frankanin99@gmail.com>
2024-06-06 03:33:58 +0000

    recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,12 +7,13 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list containing the titles of all hot articles"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"after": after, "count": count, "limit": 100}
-    response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False)
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 404:
         return None
